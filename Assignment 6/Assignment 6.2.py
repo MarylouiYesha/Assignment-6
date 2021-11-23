2bec80b434de86bb6d10cf7647835fd4857272dc
user <yesha.maryloui@gmail.com>
2021-11-23 16:57:17 +0800

    Updating Files

--- Assignment 6/Assignment 6.2.py
+++ Assignment 6/Assignment 6.2.py
@@ -31,5 +31,122 @@ if ans==sum:
 else:
     print("Your answer is wrong")
 
-print ("Total Score: " + str(score) +"/10")
+print("3. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("3. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("4. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("5. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("6. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("7. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("8. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("9. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
+print("10. Please add the following numbers")
+a=random.randint(0,99)
+b=random.randint(0,99)
+sum= a + b
+print(str(a),"+ " +str(b))
+
+ans=int(input("Answer:"))
+if ans==sum:
+    print("Your answer is correct")
+    score+= 1
+else:
+    print("Your answer is wrong")
+
 #Display the result summary of the 10 operations (ex 9/10)
+print ("Total Score: " + str(score) +"/10")
