830a17935e9ea954baa7cbe12fdb2b53a78dc39c
user <yesha.maryloui@gmail.com>
2021-11-23 13:17:29 +0800

    Uploading Files

--- Assignment 6/Assignment 6.1.py
+++ Assignment 6/Assignment 6.1.py
@@ -6,19 +6,23 @@ c=int(input("Please enter your third number:"))
 d=int(input("Please enter your fourth number:"))
 
 #Print the 4 numbers from highest to lowest using only if-else statement.
-
-if a> b and c and d:
+def arrange():    
+ if a> b and c and d:
     (first_number)=a
-
-if b>a and c and d:
+    return first_number
+ if b>a and c and d:
     (second_number)=b 
-
-if c>a and b and d:
+    return second_number
+ if c>a and b and d:
     (third_number)=c
-
-if d>a and b and c:
+    return third_number
+ if d>a and b and c:
     (fourth_number)=d
-    
+    return fourth_number
+first_number=a
+second_number =b
+third_number=c
+fourth_number=d
 
 
 print (f"{first_number},{second_number},{third_number},{fourth_number}")
