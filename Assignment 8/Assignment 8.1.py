3661bc488828c9884876d3ab3358fdb63499ec19
user <yesha.maryloui@gmail.com>
2021-12-11 02:43:57 +0800

    Uploading Files

--- Assignment 8/Assignment 8.1.py
+++ Assignment 8/Assignment 8.1.py
@@ -1,37 +1,47 @@
-#Program 1: Lottery
 import random
 import sys
-#Create a program that ask 3 numbers (0-9) from the user.
 def main_game():
-    print("Welcome to lottery game!")
-    a, b, c =input("Please input your 3 numbers (0-9) (ex: 0 5 7): ").split()
-
-#Generate 3 random winning numbers (0-9)
     rnumber = []
-    while len(rnumber) < 3:
-        random_num = random.randrange(0, 9)
-        rnumber.append(random_num)
-    print("Winning numbers are:", rnumber)
+    ynumber = []
 
-#Display “Winner” if all 3 input numbers matched the generated numbers
+    print("Welcome to lottery game!")
+    a =int(input("Please input your number: "))
+    if 0<= a <=9:
+        one_random_num = random.randrange(0, 9)
+        rnumber.append(one_random_num)
+        ynumber.append(a)
+    else:
+        print("error!")
+    
+    b = int(input("Please input your number: "))
+    if 0<= b <= 9:
+        two_random_num=random.randrange(0,9)
+        rnumber.append(two_random_num)
+        ynumber.append(b)
+    else:
+        print("error!")
+    
+    c = int(input("Please enter you number: "))
+    if 0<= c <= 9:
+        three_random_num=random.randrange(0,9)
+        rnumber.append(three_random_num)
+        ynumber.append(c)        
+    else:
+        print ("error!")
+    
 
-    if (sorted(a and b and c)) == (sorted(rnumber)):
+    print("Winning Numbers", sorted(rnumber))
+
+
+    if sorted(ynumber) == sorted(rnumber):
         print("Winner")
-#Display ”You loss” if not
     else:
         print ("You loss")
 
-
-def second_game():
-#Display ”Try again y/n” after each game
-    s_game=input ("Try again y/n ('y' if yes and 'n' if no:").lower()
-#If the user enter “y” the user will play again
-    if (s_game) == "y" or "Y":
+    s_game=input ("Try again y/n ('y' if yes and 'n' if no): ")
+    if s_game == "Y" or "y":
         main_game()
-
-#if “n” the program will exit
     else:
         sys.exit()
-       
+   
 main_game()
-second_game()
